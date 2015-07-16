c768191034ea723cfcdc916219b7802a86ec5c9b
kvinogradov <kostya.vinogradov@gmail.com>
2015-07-16 14:55:54 +0700

    Update repo

--- reports/models.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from reports.accidents.models import *
-
-
--- www/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class WwwAppConfig(AppConfig):
-    name = 'www'
-    verbose_name = 'Web Inerface'
-
-    def ready(self):
-        import signals
